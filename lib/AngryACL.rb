978cff9004cd656e1e13b20fac26fb3643587518
Mikhail Stolbov <mstolbov@gmail.com>
2012-02-12 15:54:56 +0400

    remove comments

--- lib/AngryACL.rb
+++ lib/AngryACL.rb
@@ -4,8 +4,6 @@ module AngryACL
   autoload "Acl", 'AngryACL/acl'
   autoload "Resource", 'AngryACL/resource'
   autoload "Builder", 'AngryACL/builder'
-  #autoload "Builder::Roles", "AngryACL/builder/roles"
-  # Your code goes here...
   
   def self.build(&block)
     Builder.build(&block)
